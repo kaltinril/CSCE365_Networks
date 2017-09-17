8f43ac1ece9eb2559829f32989f394783e09c9d2
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2017-09-16 18:36:48 -0800

    fixed index and handle empty string

--- hw1/server/server_ftp.py
+++ hw1/server/server_ftp.py
@@ -1,6 +1,6 @@
-from socket import *  # Used for sending and receiving information over sockets
-import shlex  # need this for it's split that keeps quoted filenames
-
+from socket import *    # Used for sending and receiving information over sockets
+import shlex            # need this for it's split that keeps quoted filenames
+import pickle           # need this for serializing objects
 
 class FTPServer:
     def __init__(self, server_port=5000):
@@ -21,20 +21,30 @@ class FTPServer:
         while True:
             message, client_address = self.server_socket.recvfrom(2048)
             modified_message = message.decode()
-            self.__check_command(modified_message)
+            self.__check_command(modified_message, client_address)
             self.server_socket.sendto(modified_message.upper().encode(), client_address)
 
     # Private methods
-    def __check_command(self, command):
-        command_and_args = shlex.split(command)
+    def __check_command(self, command, client_address):
+        if not command:
+            print("Empty command!")
+            return
 
+        # Split the string on spaces, if there are quotes, spaces inside that are preserved
+        command_and_args = shlex.split(command)
         print(command_and_args)
 
-        if command_and_args[1].lower() == "list":
+        if command_and_args[0].lower() == "list":
             print("Listing directory contents")
-        elif command_and_args[1].lower() == "get":
+            my_path = os.getcwd()
+            f_list = os.listdir(my_path)
+            serialized_list = pickle.dumps(f_list)
+            # connectionSocket.send(serialized_list)
+            # self.server_socket.sendto(modified_message.upper().encode(), client_address)
+
+        elif command_and_args[0].lower() == "get":
             print("Get a file")
-        elif command_and_args[1].lower() == "exit":
+        elif command_and_args[0].lower() == "exit":
             print("Exit")
         else:
             print("Unknown command! " + command)
